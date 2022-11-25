85c3740ac5e1d8ae4a538339a42f990925e69dca
Ernest Buffington <ernest.buffington@gmail.com>
2022-11-25 15:24:29 -0500

    Change Portal admin to 2 - it needed to reflect the 2nd user as admin

--- themes/Titanium_Core/theme.php
+++ themes/Titanium_Core/theme.php
@@ -75,9 +75,9 @@ define('THEME_DOWNLOAD_LINK', $theme_download_link);
 
 # your admin id - this will normally be 2 Set this to the MAIN ADMIN NUMBER
 global $portaladmin, $above_marquee_left, $above_marquee_right;
-$portaladmin = 3;
+$portaladmin = 2;
 echo "<!-- Setting MAIN ADMIN TO ".$portaladmin." in themes/".$theme_name."/theme.php -->\n";
-$above_marquee_left = '<span style="color:#b8a265"><strong>Welcome to PHP-Nuke Titanium, Please Enjoy Your Visit...</strong></span>';
+$above_marquee_left = '<span style="color:#b8a265"><strong>ScottyBcoder\'s Online Humble Abode, Please Enjoy Your Visit...</strong></span>';
 $above_marquee_right = '<span style="color:#b8a265"><strong>This is the Sponsor Tron for the PHP-Nuke Titanium Project...</strong></span>';
 
 /*
