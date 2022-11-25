c27b15cf76130c61f1f453e210c39eebe05abcc8
Ernest Buffington <ernest.buffington@gmail.com>
2022-11-25 14:15:10 -0500

    Initial Commit

---
+++ modules/Forums/cache/tpl2_themes.CarbinFiber-Red-Flames.forums.overall_footer.php
@@ -0,0 +1,12 @@
+<?php
+
+// eXtreme Styles mod cache. Generated on Mon, 07 Nov 2022 17:46:44 +0000 (time=1667843204)
+
+?><div style="text-align: center">		
+	<?php if ($this->vars['REPORT_LINK']) {  ?>
+	<br /><?php echo isset($this->vars['REPORT_LINK']) ? $this->vars['REPORT_LINK'] : $this->lang('REPORT_LINK'); ?>
+	<?php } ?>
+	<?php if ($this->vars['ADMIN_LINK']) {  ?>
+	<br /><?php echo isset($this->vars['ADMIN_LINK']) ? $this->vars['ADMIN_LINK'] : $this->lang('ADMIN_LINK'); ?>
+	<?php } ?>
+</div>
